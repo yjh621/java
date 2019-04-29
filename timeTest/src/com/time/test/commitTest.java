adc04388dc5aeab72b37a61f336be1066faf6999
judy621 <judy621@JUDY621-IBN1.infobank.net>
2019-04-29 14:08:32 +0900

    commitTest2

---
+++ timeTest/src/com/time/test/commitTest.java
@@ -0,0 +1,11 @@
+package com.time.test;
+
+public class commitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("commitTest");
+
+	}
+
+}
